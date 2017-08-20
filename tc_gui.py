9ce55a6a9346a8d6933b31e72c9038d2d1530bf8
DanSeraf <seraf.daniele@gmail.com>
2017-08-20 23:43:57 +0200

    gtk timer, send message on enter, just experimenting

--- tc_gui.py
+++ tc_gui.py
@@ -1,13 +1,19 @@
 import gi
+from daemon.src.tc_sessions import ClientSession
+import asyncio
 gi.require_version('Gtk', '3.0')
 from gi.repository import GObject
 from gi.repository import Gtk
 
-# we can call it just about anything we want
+
+def run_coro(coro, *args, **kwargs):
+    return asyncio.get_event_loop().run_until_complete(coro(*args, **kwargs))
+
+
 class Gui:
 
-    def on___glade_unnamed_1_destroy (self, object, data=None):
-        print ("quit with cancel")
+    def on___glade_unnamed_1_destroy(self, object, data=None):
+        print("quit with cancel")
         Gtk.main_quit()
 
     # This is the same as above but for our menu item.
@@ -17,17 +23,19 @@ class Gui:
 
     # This is our init part where we connect the signals
     def __init__(self):
+        # Connect to daemon
+        self.client = run_coro(ClientSession.connectToDaemon, host='localhost', port=8000)
         self.gladefile = "model.glade" # store the file name
         self.builder = Gtk.Builder() # create an instance of the Gtk.Builder
         self.builder.add_from_file(self.gladefile) # add the xml file to the Builder
-
         self.builder.connect_signals(self)
-
         self.window = self.builder.get_object("Window1") # This gets the 'window1' object
         self.window.show() # this shows the 'window1' object
+
         self.aboutdialog = self.builder.get_object("aboutdialog1")
-        # TextBuffer 
+
         self.chatEntry = self.builder.get_object("chatEntry")
+        # Peers         
         self.model = Gtk.ListStore (str)
         self.treeview = Gtk.TreeView(self.model)
         self.treeview = self.builder.get_object("treeView")
@@ -36,11 +44,16 @@ class Gui:
         column1 = Gtk.TreeViewColumn("peers", render1, text = 0)
         self.treeview.append_column(column1)
         self.textBox = self.builder.get_object("textBox")
+        GObject.timeout_add(500, self.updateClient)
 
+    def updateClient(self):
+        print (run_coro(self.client.getLastMessageFromDaemon))
+        return True
 
-        t = Gtk.TextBuffer()
-        t.set_text('tnoeu\nnigga')
-        self.textBox.set_buffer(t)
+    def print_on_chatbox(self, st):
+        gbuf = self.textBox.get_buffer()
+        gbuf.set_text(gbuf.get_text()+'\n'+ st)
+        self.textBox.set_buffer(gbuf)
 
     def add_peer(self, id):
         self.model.append((id))
@@ -67,7 +80,7 @@ class Gui:
     def on_chatentry_activate(self, *args, **kwargs):
         print (self.chatEntry.get_text())
         self.chatEntry.set_text('')
-
+        self.session.sendMessageToPeer
     def on_chatentry_changed(self, *args, **kwargs):
         pass
 
